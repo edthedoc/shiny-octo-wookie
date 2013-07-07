c7e652e0e5e4389b7fcc85e59696a496a8574f72
edthedoc <edthedoc@gmail.com>
2013-07-07 13:06:46 +0100

    Update config.h

--- src/config.h
+++ src/config.h
@@ -2,9 +2,9 @@
 #define CONFIG_H
 
 //NOTE: longitude is positive for East and negative for West
-#define LATITUDE    				51.0
-#define LONGITUDE 					8.0
-#define TIMEZONE 					0
+#define LATITUDE    				39.7
+#define LONGITUDE 				19.7
+#define TIMEZONE 				0
 #define DAY_NAME_LANGUAGE 			DAY_NAME_ENGLISH 		// Valid values: DAY_NAME_ENGLISH, DAY_NAME_GERMAN, DAY_NAME_FRENCH
 #define MONTH_NAME_LANGUAGE 		MONTH_NAME_ENGLISH 		
 #define MOONPHASE_NAME_LANGUAGE 	MOONPHASE_TEXT_ENGLISH 	// Valid values: MOONPHASE_TEXT_ENGLISH, MOONPHASE_TEXT_GERMAN, MOONPHASE_TEXT_FRENCH
